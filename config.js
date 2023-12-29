d893248a61695480fd1df5380710289f28864a72
Karina_mr <gatica265@gmail.com>
2023-12-29 13:40:51 -0600

    Configuración .env

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
-exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://127.0.0.1:27017/burger-queen-api';
+require('dotenv').config();
+exports.port = process.argv[2] || process.env.PORT || 8081;
+exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
